c2e73f3f5e44e87a7470e29677bab76b5720372b
Apoorva Saxena <apsaxena24@gmail.com>
2018-02-05 21:26:00 +0000

    Changes name of the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">News Flip</h1>
+          <h1 className="App-title">News Organiser</h1>
         </header>
         <Sections/>
       </div>
