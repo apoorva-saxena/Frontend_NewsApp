242c7c452ca6f4fb7bc8afe0c480eee45b54937e
Apoorva Saxena <apsaxena24@gmail.com>
2018-02-11 00:23:38 +0000

    Adds route for home

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, {Component} from 'react';
-import './App.css';
-import Sections from './Components/Sections'
+import Home from './Components/Home'
 import {BrowserRouter as Router, Route, NavLink, Link, Switch} from 'react-router-dom'
 
-class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">News Organiser</h1>
-        </header>
-        <Sections/>
-      </div>
-    );
-  }
-}
+const App = () => (
+   <Router>
+      <Route exact path="/" component={Home} />
+  </Router>
+)
 
-class App extends Component { <Router> <div>
-  <Links/>
-  <Switch>
-    <Route exact path="/" render={() => <h1>Home</h1>}/>
-    <Route path="/about" render={() => <h1>About</h1>}/>
-    <Route render={() => <h1>Page not found</h1>}/>
-  </Switch>
-</div> </Router>
-}
 
 export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Sections from '../Sections'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        <header className="home-header">
+          <h1 className="home-title">News Organiser</h1>
+        </header>
+        <Sections/>
+        <style jsx>
+          {
+            ` .home {
+              text-align: center;
+            }
+
+            .home-header {
+              background-color: #222;
+              height: 40px;
+              padding: 20px;
+              color: white;
+            }
+            .home-title {
+              font - size: 1.25e m;
+              font: reithsans;
+              float: left;
+              text - transform: uppercase;
+            }
+             `
+          }</style>
+      </div>
+    );
+  }
+}
+
+export default Home
